cf5396ca001653fb0792fc67a7e4594c712003d8
rstallman <ljl@sdust.edu.cn>
2023-02-01 10:56:46 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 李金良 on 2023/2/1.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
